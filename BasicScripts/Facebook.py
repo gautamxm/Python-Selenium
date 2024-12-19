ed5d75407259e32e112fd5461f07ccb959403a29
gautamxm <hgautam12423@gmail.com>
2024-12-19 19:45:44 +0530

    locators

---
+++ BasicScripts/Facebook.py
@@ -0,0 +1,24 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+serviceObj = Service(r"D:\BEBO\chromedriver-win64\chromedriver.exe")
+driver = webdriver.Chrome(service = serviceObj)
+
+driver.get("https://www.facebook.com/login/")
+
+beforemail = driver.find_elements(By.XPATH,"//input[@name='email']/parent::div/preceding::*")
+aftermail = driver.find_elements(By.XPATH,"//input[@name='email']/parent::div/following::*")
+time.sleep(5)
+
+print(len(beforemail)+ len(aftermail))   #213
+
+for i in beforemail:
+    print(i.tag_name,end=",")
+
+for i in aftermail:
+    print(i.tag_name,end=",")
+
+
